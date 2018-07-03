3e04c0608e067a6450f495759de379113a684f1d
Javier Gonzalez <javig56@gmail.com>
2018-07-03 16:36:27 -0700

    Stackl CSharp

---
+++ Stack.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace OOP
+{
+    public class Stack
+    {
+        private List<Node> _list = new List<Node>();
+        public Stack(){};
+        public void Push(Node node){
+            _list.Add(node);
+        }
+        public void Pop(){
+            _list.RemoveAt(_list.Count-1);
+        }
+
+    }
+    public class Node{
+        private readonly val;
+        public Node(val)
+        {
+            this.val = val;
+        }
+    }
+}

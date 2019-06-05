9b11acadbc2d5dc0b4116fb448e288a2e3e3e852
Javier Gonzalez <javig56@gmail.com>
2019-06-04 21:34:04 -0700

    Test2 Interface file test

--- Interface.cs
+++ Interface.cs
@@ -4,14 +4,14 @@ namespace OOP
 {
     public interface IShippingCalculator{
         //When members implemented should be with Public modifier
-        float CalculateShipping(Order order);
+        // float CalculateShipping(Order order);
     }
     public class ShippingCalculator : IShippingCalculator{
-        public float CalculateShipping(Order order){
-            if(order.TotalPrice < 30f)
-                return order.TotalPrice * .1f;
+        // public float CalculateShipping(Order order){
+        //     if(order.TotalPrice < 30f)
+        //         return order.TotalPrice * .1f;
 
-            return 0;
-        }
+        //     return 0;
+        // }
     }
 }
